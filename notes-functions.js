bf8cc1bc3e4476619f4fa0083dbcfb1a92702d79
Tal Taitelbaum <talbak.tb@gmail.com>
2022-11-29 08:00:22 +0200

    notes project

---
+++ notes-functions.js
@@ -0,0 +1,109 @@
+//Read existing from localStorage; Read it, check it, parse it
+
+const getSavedNotes = () => {
+  const notesJSON = localStorage.getItem("notes");
+
+  if (notesJSON !== null) {
+    return JSON.parse(notesJSON);
+  } else {
+    return [];
+  }
+};
+
+//Remove a note from the list
+const removeNote = (id) => {
+  const noteIndex = notes.findIndex((note) => note.id === id);
+
+  if (noteIndex > -1) {
+    notes.splice(noteIndex, 1);
+  }
+};
+
+//Save the notes to localStorage
+const saveNotes = () => {
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+//Generate the DOM structure for a new note
+
+const generateNoteDOM = (note) => {
+  const noteEl = document.createElement("div");
+  const textEL = document.createElement("a");
+  const button = document.createElement("button");
+
+  //Setup the remove note button
+  button.textContent = "X";
+  noteEl.appendChild(button);
+  button.addEventListener("click", () => {
+    removeNote(note.id);
+    saveNotes(notes);
+    renderNotes(notes, filters);
+  });
+
+  //setup the note title text
+  if (note.title.length > 0) {
+    textEL.textContent = note.title;
+  } else {
+    textEL.textContent = "Unnamed note";
+  }
+
+  textEL.setAttribute("href", `./edit.html#${note.id}`);
+  noteEl.appendChild(textEL);
+
+  return noteEl;
+};
+
+//Sort your notes by one of three ways
+sortNotes = (notes, sortBy) => {
+  if (sortBy === "byEdited") {
+    return notes.sort((a, b) => {
+      if (a.updatedAt > b.updatedAt) {
+        return -1;
+      } else if (a.updatedAt < b.updatedAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === "byCreated") {
+    return notes.sort((a, b) => {
+      if (a.createdAt > b.createdAt) {
+        return -1;
+      } else if (a.createdAt < b.createdAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === "alphabetical") {
+    return notes.sort((a, b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+    return notes;
+  }
+};
+
+//Render application notes
+const renderNotes = (notes, filters) => {
+  notes = sortNotes(notes, filters.sortBy);
+  const filteredNotes = notes.filter((note) =>
+    note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+  );
+
+  document.querySelector("#notes").innerHTML = "";
+
+  filteredNotes.forEach((note) => {
+    const notEl = generateNoteDOM(note);
+    document.querySelector("#notes").appendChild(notEl);
+  });
+};
+
+//Generate the last edited message
+const generateLastEdited = (timestamp) =>
+  `last edited ${moment(timestamp).fromNow()}`;
---
+++ some-file.js
@@ -0,0 +1,23 @@
+// Unix Epoch -January 1st 1970 00:00:00
+const now = new Date();
+const timestamp = now.getTime();
+const myDate = new Date(timestamp);
+console.log(myDate.getFullYear());
+
+// console.log(`Year: ${now.getFullYear()}`);
+// console.log(`Month: ${now.getMonth()}`);
+// console.log(`Day of the month: ${now.getDate()}`);
+// console.log(`Hour: ${now.getHours()}`);
+// console.log(`Minutes: ${now.getMinutes()}`);
+// console.log(`Seconds: ${now.getSeconds()}`);
+
+const dateOne = new Date("March 1 2017 12:00:00");
+const dateTwo = new Date();
+const dateOneTimestamp = dateOne.getTime();
+const dateTwoTimestamp = dateTwo.getTime();
+
+if (dateOneTimestamp < dateTwoTimestamp) {
+  console.log(dateOne.toString());
+} else if (dateTwoTimestamp < dateOneTimestamp) {
+  console.log(dateTwo.toString());
+}
